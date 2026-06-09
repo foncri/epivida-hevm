caf8bad9d54e650b464360cc5ce577125658bc78
foncri <todofoncri@gmail.com>
2026-06-09 08:40:05 -0600

    Configure EPIVIDA Lite Firebase web app

--- lite/epivida-lite-config.js
+++ lite/epivida-lite-config.js
@@ -1,10 +1,10 @@
 window.EPIVIDA_LITE_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAYFRoQiPhS8jXTefHcA9PfmbSbT4WNdNw",
+  authDomain: "epivida-hevm.firebaseapp.com",
+  projectId: "epivida-hevm",
+  storageBucket: "epivida-hevm.firebasestorage.app",
+  messagingSenderId: "721538111286",
+  appId: "1:721538111286:web:d99ecc9fa13874600d4597"
 };
 
 window.EPIVIDA_LITE_REQUIRE_AUTH = true;
